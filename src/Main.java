0a15648ed24b23b6835e0bc9f2d83912a5241af6
tassiosantos <tassiosantos@gmail.com>
2023-06-26 21:27:51 -0300

    Exercise finished

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        //Solução 1
 //        double area, pi, raio;
 //        pi = 3.14159;
 //        raio = 2.001;
 //        area = pi*raio*raio;
 //        System.out.println("A = " + area);
 
-        String numMatricula ="123456";
+        //Solução 2
+//        String numMatricula;
+//        int horasTrabalhadas;
+//        double valorHora, salario;
+//
+//        numMatricula = "123456";
+//        horasTrabalhadas = 20;
+//        valorHora = 17.6f;
+//        salario = horasTrabalhadas*valorHora;
+//
+//        System.out.printf("Matrícula = %s%nSalário = R$ %.2f%n", numMatricula, salario);
 
+        //Solução 3
+//        String nome;
+//        Double salarioFixo, vendasMes, salarioFinal;
+//
+//        nome = "Matheus";
+//        salarioFixo = 500.00;
+//        vendasMes = 1230.30;
+//        salarioFinal = salarioFixo + vendasMes*(.15);
+//
+//        System.out.printf("TOTAL = R$ %.2f",salarioFinal);
 
     }
 }
