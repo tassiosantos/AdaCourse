3b9731e648371b335ac4c9cf07bbe3a5a632d09f
tassiosantos <tassiosantos@gmail.com>
2023-06-26 21:01:55 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        double area, pi, raio;
+//        pi = 3.14159;
+//        raio = 2.001;
+//        area = pi*raio*raio;
+//        System.out.println("A = " + area);
+
+        String numMatricula ="123456";
+
+
+    }
+}
